0f5a8aa7a3f5c25713fc6cd31df3a9aff9be4a5f
Lior229 <lior229@gmail.com>
2023-02-26 18:04:45 +0200

    lior first try - project2

--- Project2/main.js
+++ Project2/main.js
@@ -1,8 +1,10 @@
 const COINS_API_URL = "https://api.coingecko.com/api/v3/coins/list";
 const COINS_API_BY_ID = "https://api.coingecko.com/api/v3/coins/";
 const localStorageKey = 'CoinsKey'
+const localStorageReportKey = 'ReportCoinsKey'
 
 let coins;
+let reportCoins;
 
 const navigations = {
     'home-link': 'home.html',
@@ -12,11 +14,9 @@ const navigations = {
 
 $(async ()=> { 
 
-    await getCoins();
+    await showPage("home.html");
 
-    displayCoins(coins);
-
-    $("a").click(function (event) { 
+    $("a").on("click", async function (event) { 
 
         //Prevent a link from opening the URL
         event.preventDefault()
@@ -24,10 +24,21 @@ $(async ()=> {
         const linkId = this.id
         const url = navigations[linkId]
 
-        showPage(url)
+        await showPage(url);
+
     });
 
-    $(".moreInfo").click(function (event) { 
+    registerEventListeners()
+    
+});
+
+function registerEventListeners() {
+    $(".moreInfo").off("click")
+    $("#search-btn").off("click")
+    $(":checkbox").off("change")
+
+    $(".moreInfo").on("click", function (event) { 
+        console.log('heyyyyy');
 
         const clicked_coin_id = $(this).attr('data-coin-id')
 
@@ -47,9 +58,9 @@ $(async ()=> {
                 let moreInfoHtml = `<div class="card card-body">
                                         <img src="${coin_img}" alt="">
                                         <br>
-                                        <p class="card-text"> UDS ${usd_price}</p>
-                                        <p class="card-text"> LIS ${ils_price}</p>
-                                        <p class="card-text"> EUR ${eur_price}</p>
+                                        <p class="card-text"> UDS ${usd_price}&dollar;</p>
+                                        <p class="card-text"> LIS ${ils_price}&#8362</p>
+                                        <p class="card-text"> EUR ${eur_price}&euro;</p>
                                     </div>
                                     `;
 
@@ -62,8 +73,78 @@ $(async ()=> {
         });
 
     });
-    
-});
+
+    $("#search-btn").on("click", function() {
+        const symbolToSearch = $("#searchBox").val().toLowerCase();
+
+        // !symbolblabla
+        if (symbolToSearch.length < 1) {
+            //display all coins if there is no string to search
+            displayCoins(coins);
+        } else {
+            const result = coins.filter((coin) => {
+                return coin.symbol.includes(symbolToSearch);
+            });
+
+            if (result.length < 1) {
+                //display all coins if there no search result 
+                $(".coinCardContainer").show()
+            } else {
+                $(".coinCardContainer").hide()
+
+                for (const coin of result) {
+                    $(`#coinCardContainer-${coin.id}`).show();
+                }
+            }
+
+        }
+    });
+
+    $(":checkbox").on("change", function() {
+
+        const coin = coins.find((coin) => {
+            return coin.id === this.id.replace('reportCheckbox-','');
+        });
+
+        if(this.checked) {
+            if (reportCoins.length < 5) {
+                reportCoins.push(coin);
+            } else {
+                //todo add the select coin ( th 6)
+                // switch with other coin -add button 
+                //todo: clear coin-list
+                // no need close and X
+                $("#coin-list").html(`<p>You can choose up to 5 coins.</p>
+                                      <p>coin:</p>
+                                        <p>Coin symbol: ${coin.symbol}, Coin name: ${coin.name}</p>
+                                        <p>you have already select the coins:</p>
+                `);
+                let ReportCoinHTML = `<ul class="list-group">`;
+
+                for (const reportCoin of reportCoins){
+                    ReportCoinHTML += `<li class="list-group-item">
+                                            <div>
+                                                <p>Coin symbol: ${reportCoin.symbol}, Coin name: ${reportCoin.name}</p>
+                                                <button class="btn btn-warning" type="button">switch</button>
+                                            </div>
+                                        </li>`;
+                }
+
+                ReportCoinHTML += `</ul>`;
+                $("#coin-list").append(ReportCoinHTML);
+                $("#myModal").modal("show");
+                this.checked = false;
+            }
+        } else {
+            // Remove coind from report arry
+            reportCoins.splice(reportCoins.indexOf(coin), 1);
+        }
+
+        // update loacl storage after change
+        localStorage.setItem(localStorageReportKey, JSON.stringify(reportCoins));
+    });
+
+}
 
 function getCoinsFromApi(){
 
@@ -76,31 +157,60 @@ function getCoinsFromApi(){
     })
 }
 
-function loadCoinsFromLocalStorage() {
+function loadCoinsFromLocalStorage(storageKey) {
 
-    const str = localStorage.getItem(localStorageKey);
+    const str = localStorage.getItem(storageKey);
   
     const coins = (str === null) ? [] : JSON.parse(str);
   
     return coins;
 }
 
-function showPage(url) {
-    $.ajax({
-        url: url,
-        success: function (response) {      
-            $("#root").html(response);
-        },
-        error: function (err){
-            console.log(err.statusText, err.status);
-        }
-    });
+function getPage(url) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            success: function (response) {      
+                resolve(response)
+            },
+            error: function (err){
+                reject(err)
+            }
+        });    
+    })
+    
+}
 
-  }
+async function showPage(url) {
+    const htmlPage = await getPage(url)
+    $("#root").html(htmlPage)
+    await handlePageChange(url) // make sure we still wait for coins from api in case its not in localstorage before registering the events
+}
+
+async function handlePageChange(url) {
+
+    console.log(url);
+    switch (url) {
+        case navigations["home-link"]:
+            console.log("case home");
+                await getCoins();
+                getReportCoins();
+                displayCoins(coins); 
+                registerEventListeners()
+            break;
+        case navigations["report-link"]:
+
+            break;
+        case navigations["about-link"]:
+        default:
+            break;
+    }
+
+}
 
 async function getCoins() {
 
-    coins = loadCoinsFromLocalStorage();
+    coins = loadCoinsFromLocalStorage(localStorageKey);
 
     if (coins.length === 0) {
 
@@ -110,37 +220,58 @@ async function getCoins() {
             console.log(error);
         }
 
+        // TODO: delete silce  - 100 coins for testing
+        coins = coins.slice(0,100);
+
         localStorage.setItem(localStorageKey, JSON.stringify(coins));
     }
+
+
+}
+
+async function getReportCoins() {
+    reportCoins = loadCoinsFromLocalStorage(localStorageReportKey);
 }
 
 function displayCoins(coins) {
 
     let cards = `<div class="row">`;
 
-    for (let index = 0; index < 100; index++) {
+    for (const coin of coins) {
 
-         cards += `<div class="col">
-                        <div class="card myCardStyle">
-                            <div class="card-body position-relative">
-                                <div class="form-check form-switch position-absolute top-0 end-0">
-                                    <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                                </div>
-                                <h5 class="card-title">${coins[index].symbol}</h5>
-                                <p class="card-text">${coins[index].name}</p>
-                                <button class="moreInfo btn btn-primary" data-coin-id=${coins[index].id} type="button" data-bs-toggle="collapse" data-bs-target="#moreInfo-${coins[index].id}">
-                                  More info
-                                </button>
-                                <div class="collapse" id="moreInfo-${coins[index].id}">
-                                </div>
+        //if coin selected for report show checkbox as checked
+        let checked = "";
+
+        if (reportCoins.length > 0) {
+
+            const result = reportCoins.find((coinToFind) => {
+                return coin.id === coinToFind.id;
+            });
+
+            checked = result ? "checked" : "";
+        }
+
+        cards += `<div class="col coinCardContainer" id="coinCardContainer-${coin.id}" >
+                    <div class="card myCardStyle">
+                        <div class="card-body position-relative">
+                            <div class="form-check form-switch position-absolute top-0 end-0">
+                                <input class="form-check-input" type="checkbox" role="switch" id="reportCheckbox-${coin.id}" ${checked}>
+                            </div>
+                            <h5 class="card-title">${coin.symbol}</h5>
+                            <p class="card-text">${coin.name}</p>
+                            <button class="moreInfo btn btn-primary" data-coin-id=${coin.id} type="button" data-bs-toggle="collapse" data-bs-target="#moreInfo-${coin.id}">
+                                More info
+                            </button>
+                            <div class="collapse" id="moreInfo-${coin.id}">
                             </div>
                         </div>
-                    </div>` ;
+                    </div>
+                </div>` ;
         
     }
 
     cards += `</div>`
 
-    $('#card-container').html(cards);
-    
+    $("#card-container").html(cards);
+    $("#card-container").show();
 }
