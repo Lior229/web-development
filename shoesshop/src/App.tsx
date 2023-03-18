06a3668f53af2744e95193ed2a1b362ae918fd9f
Lior229 <lior229@gmail.com>
2023-03-18 15:29:33 +0200

    order 18-3

--- shoesshop/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import LayoutArea from './components/LayoutArea/LayoutArea';
-import './App.css';
-
-function App() {
-    return <LayoutArea />
-}
-
-export default App;
