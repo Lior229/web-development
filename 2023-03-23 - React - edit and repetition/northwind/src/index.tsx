11b9f16b1990cba1906b1486e1db74589e95e630
Lior229 <lior229@gmail.com>
2023-06-15 12:09:04 +0300

    try

--- 2023-03-23 - React - edit and repetition/northwind/src/index.tsx
+++ 2023-03-23 - React - edit and repetition/northwind/src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
-import store from './store'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -10,13 +8,14 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
+
+
+
 root.render(
     // <React.StrictMode>
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    // </Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 
     // </React.StrictMode>
 );
